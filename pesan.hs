19ca4877c4669e649263f96793c95bf9d9dba173
Speedrun <86620608+Bayu12345677@users.noreply.github.com>
2021-12-19 18:02:29 +0700

    Update pesan.hs

--- pesan.hs
+++ pesan.hs
@@ -1,4 +1,4 @@
 
-PutStrLn "kagak punya skill kebanyakan bacot"
-PutStrLn "skill depes udah merasa bangga bangsat webshell ae masi minta"
-PutStrLn "testi ini karya w apa tidak pm : 6285731184377"
+putStrLn "kagak punya skill kebanyakan bacot"
+putStrLn "skill depes udah merasa bangga bangsat webshell ae masi minta"
+putStrLn "testi ini karya w apa tidak pm : 6285731184377"
