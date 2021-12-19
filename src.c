8e81b0a59b7410ccb5bfff83bf4a575c5f0a309a
Speedrun <86620608+Bayu12345677@users.noreply.github.com>
2021-12-19 14:31:16 +0700

    Create src.c

---
+++ src.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+// depes elu maa termux masi beda kerl 
+// panas chat gua ae kerl kita diskusi bersama 6285731184377
+
+int main(){
+     printf("skill depes sok ngatain dev app mobile masi dangkal kerl skill elu");
+}
