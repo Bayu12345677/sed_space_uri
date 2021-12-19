230046187109fdadd13b94ed7a5faaf446f93eab
Speedrun <86620608+Bayu12345677@users.noreply.github.com>
2021-12-19 15:53:53 +0700

    Update src.c

--- src.c
+++ src.c
@@ -2,6 +2,7 @@
 
 // depes elu maa termux masi beda kerl 
 // panas chat gua ae kerl kita diskusi bersama 6285731184377
+// -)
 
 int main(){
      printf("skill depes sok ngatain dev app mobile masi dangkal kerl skill elu");
