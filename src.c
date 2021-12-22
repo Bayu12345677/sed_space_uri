782b4b8cf1622fbf80f18ba441049fcc47442f05
Speedrun <86620608+Bayu12345677@users.noreply.github.com>
2021-12-22 16:58:20 +0700

    Delete src.c

--- src.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-// depes elu maa termux masi beda kerl 
-// panas chat gua ae kerl kita diskusi bersama 6285731184377
-// -)
-
-int main(){
-     printf("skill depes sok ngatain dev app mobile masi dangkal kerl skill elu");
-}
